0110c35e5708b7885114d14f998f48d54880785b
SergeyAlexandrowich <121849648+SergeyAlexandrowich@users.noreply.github.com>
2023-05-04 07:12:04 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const btn=document.querySelector("#mainbutton");
+btn.onclick=function(){
+	document.getElementById("menu").style.display="block";
+
+}
+
+const btn2=document.querySelector("#button1");
+btn2.onclick=function(){
+	document.getElementById("menu").style.display="none";
+
+}
